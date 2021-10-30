b790bc04e76277cfa76fe49f049515392f2fb785
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-30 12:50:53 +0300

    running neighbor tests

--- jiraniapp/tests.py
+++ jiraniapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import *
 
+import datetime as dt
 # Create your tests here.
+class neighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.kibra = neighbourhood(neighbourhood='kibra')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kibra,neighbourhood))
+
+    def tearDown(self):
+        neighbourhood.objects.all().delete()
+
+    def test_save_method(self):
+        self.kibra.save_neighbourhood()
+        jirani = neighbourhood.objects.all()
+        self.assertTrue(len(jirani)>0)
--- neighborp/settings.py
+++ neighborp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
+    'bootstrap5',
+    'django_registration',
+    'jiraniapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'neighborp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'neighbor',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'127.0.0.1',
+        'PORT':'', 
     }
 }
 
