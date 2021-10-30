99b3d7d7769779425e7450f178cbb00d44a13036
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-30 12:12:22 +0300

    add neighbour model

--- jiraniapp/models.py
+++ jiraniapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+Priority=(
+    ('Low Attention','Low Attention'),
+    ('High Attention','High Attention'),
+)
+
+class neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
