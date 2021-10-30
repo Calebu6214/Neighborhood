18eb45a7edf67101eddef205c8bb41f4a8af19c9
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-30 13:44:45 +0300

    add business and notification form

--- jiraniapp/forms.py
+++ jiraniapp/forms.py
@@ -5,3 +5,13 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['username']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['owner','neighbourhood']
+    
+class notificationsForm(forms.ModelForm):
+    class Meta:
+        model=notifications
+        exclude=['author','neighbourhood','post_date']
