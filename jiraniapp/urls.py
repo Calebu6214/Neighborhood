b76e398b27781eb4aeb35d0f1eb94e01fba71138
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-30 13:48:50 +0300

    add profiles functions

--- jiraniapp/urls.py
+++ jiraniapp/urls.py
@@ -4,7 +4,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
 urlpatterns = [
-   path('',views.index,name='Index'),
+    path('',views.index,name='Index'),
+    path('my-profile/',views.my_profile, name='my-profile'),
+    path('user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    path('create/profile',views.create_profile, name='create-profile'),    
+    path('update/profile',views.update_profile, name='update-profile'),
     
 ]
 
--- jiraniapp/views.py
+++ jiraniapp/views.py
@@ -14,3 +14,29 @@ def index(request):
     except ObjectDoesNotExist:
         return redirect('create-profile')
     return render(request,'index.html')
+
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user=request.user
+    profile =Profile.objects.get(username=current_user)
+    return render(request,'profile/user_profile.html',{"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile =Profile.objects.get(username=user)
+
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user=request.user
+    if request.method=="POST":
+        form =ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+        return redirect('/')
+    else:
+        form = ProfileForm()
+        return render(request,'profile/profile_form.html',{"form":form})
