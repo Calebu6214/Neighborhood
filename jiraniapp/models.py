47a5d08ad74c8eb55a096dd74dc67ae893cfbcef
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-30 12:18:55 +0300

    add profile model

--- jiraniapp/models.py
+++ jiraniapp/models.py
@@ -21,3 +21,14 @@ class neighbourhood(models.Model):
     @classmethod
     def delete_neighbourhood(cls,neighbourhood):
         cls.objects.filter(neighbourhood=neighbourhood).delete()
+
+class Profile(models.Model):
+    profpic = models.ImageField(upload_to='profilep/')
+    description = HTMLField()
+    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
