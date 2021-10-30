78565e5cd1a3ecbca073461811ac0b720a15dd6b
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-30 13:12:06 +0300

    add homepage function

--- jiraniapp/views.py
+++ jiraniapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import *
 
 # Create your views here.
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+    return render(request,'index.html')
